b3eb4c5c499f02c8057327fba9410c481472cd39
Enrique Aguilera <enriqueaguilera1999@gmail.com>
2025-02-23 23:38:10 -0300

    feat: add functionality to load games from the API

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,33 @@
 <script setup>
 import LayoutHero from './components/Layout/LayoutHero.vue';
+import { onMounted, reactive } from 'vue'
+
+const API_URL = "https://gamestreamapi.herokuapp.com/api/games"
+
+const state = reactive({
+  error: null,
+  isLoading: false,
+  data: []
+})
+
+const fetchGames = async () => {
+  try {
+    state.isLoading = true
+    const responde = await fetch(API_URL)
+    const data = await responde.json()
+    state.data = data
+  } catch (error) {
+    console.error(error)
+    state.error = error
+  } finally {
+    state.isLoading = false
+  }
+}
+
+onMounted(()=> {
+  fetchGames()
+})
+
 </script>
 
 <template>
