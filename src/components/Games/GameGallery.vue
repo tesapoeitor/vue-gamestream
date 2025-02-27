f2738926f697bd301d16eee0fa0e2e5aae1fe492
Enrique Aguilera <enriqueaguilera1999@gmail.com>
2025-02-26 23:16:13 -0300

    feat: refactor GameGallery component to use dynamic icon components for navigation

--- src/components/Games/GameGallery.vue
+++ src/components/Games/GameGallery.vue
@@ -10,6 +10,11 @@ const props = defineProps({
   },
 })
 
+const icons = {
+  left: IconChevronLeft,
+  right: IconChevronRight
+}
+
 const currentIndex = ref(0)
 
 function nextImage() {
@@ -25,10 +30,10 @@ function prevImage() {
   <div class="game-gallery">
     <div class="game-gallery__controls">
       <button class="game-gallery__button game-gallery__button--left" @click="prevImage">
-        <IconChevronLeft />
+        <component :is="icons.left" />
       </button>
       <button class="game-gallery__button game-gallery__button--right" @click="nextImage">
-        <IconChevronRight />
+        <component :is="icons.right" />
       </button>
     </div>
 
